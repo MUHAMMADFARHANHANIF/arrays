0e25169358abe900e6daaa8409c582c09ccd7c8f
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-03-07 21:49:36 -0800

    array

---
+++ array/app.js
@@ -0,0 +1,69 @@
+
+
+// let cities = ['karachi', 'lahore','islamabad' ,['sadar', 'korangi', 'malir', 'gulshan'],90,78,true]
+// console.log(cities[3][2]);
+// console.log(typeof(cities));
+// console.log(cities.length);
+// cities[2] = "farhan"
+// console.log(cities[2]);
+
+
+
+
+
+
+
+
+const fruits = ['banana', 'mango', 'orange', 'watermelon', 'graphs']
+
+// fruits.push('anar'); ////add last
+// fruits.pop() ; //// remove last
+// fruits.unshift('farhan') //// add first
+// fruits.shift() //// remove first
+
+// fruits.splice(1,1,'farhan','hanif','farooq') 
+//// variableName.splice(first number jaha se start karna he  , kitne delet karne he , or jo add karna ho wo likho is me '' ) 
+// fruits[4].pop();
+
+console.log(fruits);
+console.log(fruits.length);
+
+
+
+
+ 
+const newArr = fruits.slice(2)
+console.log(newArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const names = 'Muhammad Abdullah khan';
+// const arr = names.split(' ')
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+// const names = ['Muhammad' , 'Abdullah' , 'Khan'];
+// const str = names.join(' ')
+// console.log(str);
